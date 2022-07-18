c553dcbdeaadec3c5130cfa41011fa48474fa290
flave1 <favouremmanuel433@gmail.com>
2022-07-18 17:13:18 +0100

    ..

---
+++ BLL/Constants/Platforms.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMP.BLL.Constants
+{
+    public enum Platforms
+    {
+        Android = 1,
+        DesktopWeb = 2,
+        IOS = 3,
+        TabletWeb = 4,
+        TablietMobile = 5,
+        AndroidWeb = 6,
+    }
+}
