30b37645db848966e8476b756e2b04006300c9b5
flave1 <favouremmanuel433@gmail.com>
2022-07-19 13:40:30 +0100

    model review

--- DAL/Models/PinManagement/UsedPin.cs
+++ DAL/Models/PinManagement/UsedPin.cs
@@ -1,19 +1,31 @@
-﻿using System;
+﻿using DAL;
+using DAL.SessionEntities;
+using DAL.StudentInformation;
+using SMP.DAL.Models.SessionEntities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SMP.DAL.Models.PinManagement
 {
-    public class UsedPin
+    public class UsedPin : CommonEntity
     {
         [Key]
         public Guid UsedPinId { get; set; }
-        public string UploadedPin { get; set; }
+        public DateTime DateUsed { get; set; }
+        public Guid StudentContactId { get; set; }
+        [ForeignKey("StudentContactId")]
+        public StudentContact Student { get; set; }
+        public Guid SessionId { get; set; }
+        [ForeignKey("SessionId")]
+        public Session Session { get; set; }
+        public Guid SessionTermId { get; set; }
+
+        [ForeignKey("SessionTermId")]
+        public SessionTerm Sessionterm { get; set; }
+        public Guid UploadedPinId { get; set; }
         [ForeignKey("UploadedPinId")]
-        public UploadedPin Pin { get; set; }
+        public UploadedPin UploadedPin { get; set; }
     }
 }
