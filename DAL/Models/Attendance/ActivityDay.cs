af6f941aa95f317fe354cb10f3d518c795e5f1cd
Olayinka Emmanuel <olayinkaemmanuel52@gmail.com>
2022-08-15 22:38:20 +0100

    #SP-97 feat(smp):  Implement Attendance System

---
+++ DAL/Models/Attendance/ActivityDay.cs
@@ -0,0 +1,23 @@
+﻿using DAL;
+using DAL.ClassEntities;
+using DAL.StudentInformation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMP.DAL.Models.Attendance
+{
+    public class ActivityDay: CommonEntity
+    {
+        [Key]
+        public Guid ActivityDayId { get; set; }
+        public string Day { get; set; }
+        public Guid StudentContactId { get; set; }
+        [ForeignKey("StudentContactId")]
+        public StudentContact StudentContact { get; set; }
+    }
+}
---
+++ DAL/Models/Attendance/ClassActivity.cs
@@ -0,0 +1,25 @@
+﻿using DAL.StudentInformation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMP.DAL.Models.Attendance
+{
+    public class ClassActivity
+    {
+        public Guid ClassActivityId { get; set; }
+        public string Activity { get; set; }
+        public Guid ActivityDayId { get; set; }
+        [ForeignKey("ActivityDayId")]
+        public ActivityDay ActivityDay { get; set; }
+        public Guid ActivityPeriodId { get; set; }
+        [ForeignKey("ActivityPeriodId")]
+        public ActivityPeriod ActivityPeriod { get; set; }
+        public Guid StudentContactId { get; set; }
+        [ForeignKey("StudentContactId")]
+        public StudentContact StudentContact { get; set; }
+    }
+}
