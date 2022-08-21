1e438d64c30a690b59730c451936475178d8a564
flave1 <favouremmanuel433@gmail.com>
2022-08-21 13:37:25 +0100

    ..

--- BLL/Services/TimetableServices/TimeTableService.cs
+++ BLL/Services/TimetableServices/TimeTableService.cs
@@ -8,7 +8,6 @@ using SMP.DAL.Models.Timetable;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace SMP.BLL.Services.TimetableServices
