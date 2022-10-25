00a0c4733f4a24ace0966d00ea1975863ff0dcea
egunfavour <egunfavour@gmail.com>
2022-10-25 17:15:28 +0100

    first commit

---
+++ BLL/Services/ParentServices/IParentService.cs
@@ -0,0 +1,15 @@
+﻿using BLL;
+using BLL.Filter;
+using BLL.Wrappers;
+using SMP.Contracts.Parents;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+namespace SMP.BLL.Services.ParentServices
+{
+    public interface IParentService
+    {
+        Task<APIResponse<UpdateParent>> AddParentAsync(Parents parent);
+        Task<APIResponse<UpdateParent>> UpdateParent(UpdateParent ParentDetail);
+        Task<APIResponse<PagedResponse<List<Parents>>>> GetAllParentsAsync(PaginationFilter filter)
+    }
+}
---
+++ DAL/Models/Parents/Parents.cs
@@ -0,0 +1,35 @@
+﻿using DAL;
+using DAL.Authentication;
+using DAL.StudentInformation;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace SMP.Contracts.Parents
+{
+    public class Parents : AppUser
+    {
+        [Key]
+        public string ParentId { get; set; }
+        public int Status { get; set; }
+        public string Relationship { get; set; }
+        public ICollection<StudentContact> Students { get; set; }
+        public IFormFile Photo { get; set; }
+    }
+    public class UpdateParent
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public string Phone { get; set; }
+        public string DOB { get; set; }
+        public IFormFile Photo { get; set; }
+        public string ImagePath { get; set }
+        public string ParentId { get; set; }
+        public string Email { get; set; }
+    }
+}
