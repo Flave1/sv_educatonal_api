e0d1fc3931cc5939fb812769ab4822d33765aa03
Olayinka Emmanuel <olayinkaemmanuel52@gmail.com>
2023-01-31 00:20:34 +0100

    updates on teachers controller

--- BLL/Services/SubjectServices/SubjectService.cs
+++ BLL/Services/SubjectServices/SubjectService.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using SMP.BLL.Constants;
 using SMP.BLL.Utilities;
 using SMP.Contracts.Common;
+using SMP.DAL.Migrations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,11 +20,13 @@ namespace BLL.Services.SubjectServices
     {
         private readonly DataContext context;
         private readonly IHttpContextAccessor accessor;
+        private readonly string smsClientId;
 
         public SubjectService(DataContext context, IHttpContextAccessor accessor)
         {
             this.context = context;
             this.accessor = accessor;
+            smsClientId = accessor.HttpContext.User.FindFirst(x => x.Type == "smsClientId")?.Value;
         }
 
         async Task<APIResponse<Subject>> ISubjectService.CreateSubjectAsync(ApplicationLookupCommand subject)
@@ -32,7 +35,7 @@ namespace BLL.Services.SubjectServices
 
             try
             {
-                if (context.Subject.AsEnumerable().Any(r => Tools.ReplaceWhitespace(subject.Name) == Tools.ReplaceWhitespace(r.Name)))
+                if (context.Subject.AsEnumerable().Any(r => Tools.ReplaceWhitespace(subject.Name) == Tools.ReplaceWhitespace(r.Name) && r.ClientId == smsClientId))
                 {
                     res.Message.FriendlyMessage = "Subject Name Already exist";
                     return res;
@@ -65,13 +68,13 @@ namespace BLL.Services.SubjectServices
 
             try
             {
-                if (context.Subject.AsEnumerable().Any(r => Tools.ReplaceWhitespace(Name) == Tools.ReplaceWhitespace(r.Name) && r.SubjectId != Guid.Parse(Id)))
+                if (context.Subject.AsEnumerable().Any(r => Tools.ReplaceWhitespace(Name) == Tools.ReplaceWhitespace(r.Name) && r.SubjectId != Guid.Parse(Id) && r.ClientId == smsClientId))
                 {
                     res.Message.FriendlyMessage = "Subject Name Already exist";
                     return res;
                 }
 
-                var lookup = context.Subject.FirstOrDefault(r => r.SubjectId == Guid.Parse(Id));
+                var lookup = context.Subject.FirstOrDefault(r => r.SubjectId == Guid.Parse(Id) && r.ClientId == smsClientId);
                 if (lookup == null)
                 {
                     res.Message.FriendlyMessage = "Subject  does not exist";
@@ -97,7 +100,7 @@ namespace BLL.Services.SubjectServices
         async Task<APIResponse<List<GetApplicationLookups>>> ISubjectService.GetAllSubjectsAsync()
         {
             var res = new APIResponse<List<GetApplicationLookups>>();
-            var result =  await context.Subject.OrderByDescending(d => d.CreatedOn).Where(d => d.Deleted != true).Select(a => new GetApplicationLookups { LookupId = a.SubjectId.ToString().ToLower(), Name = a.Name, IsActive = a.IsActive }).ToListAsync();
+            var result =  await context.Subject.Where(d => d.Deleted != true && d.ClientId == smsClientId).OrderByDescending(d => d.CreatedOn).Select(a => new GetApplicationLookups { LookupId = a.SubjectId.ToString().ToLower(), Name = a.Name, IsActive = a.IsActive }).ToListAsync();
             res.Result = result;
             res.IsSuccessful = true;
             return res;
@@ -106,7 +109,7 @@ namespace BLL.Services.SubjectServices
         async Task<APIResponse<List<GetApplicationLookups>>> ISubjectService.GetAllActiveSubjectsAsync()
         {
             var res = new APIResponse<List<GetApplicationLookups>>();
-            var result = await context.Subject.OrderByDescending(d => d.CreatedOn).Where(d => d.Deleted != true && d.IsActive == true).Select(a => new GetApplicationLookups { LookupId = a.SubjectId.ToString().ToLower(), Name = a.Name, IsActive = a.IsActive }).ToListAsync();
+            var result = await context.Subject.Where(d => d.Deleted != true && d.IsActive == true && d.ClientId == smsClientId).OrderByDescending(d => d.CreatedOn).Select(a => new GetApplicationLookups { LookupId = a.SubjectId.ToString().ToLower(), Name = a.Name, IsActive = a.IsActive }).ToListAsync();
             res.Result = result;
             res.IsSuccessful = true;
             return res;
@@ -119,13 +122,13 @@ namespace BLL.Services.SubjectServices
             
             foreach(var Id in request.Items)
             {
-                var lookup = context.Subject.FirstOrDefault(d => d.SubjectId == Guid.Parse(Id));
+                var lookup = context.Subject.FirstOrDefault(d => d.SubjectId == Guid.Parse(Id) && d.ClientId == smsClientId);
                 if (lookup == null)
                 {
                     res.Message.FriendlyMessage = "Subject  does not exist";
                     return res;
                 }
-                if(context.SessionClassSubject.Any(x => x.SubjectId == lookup.SubjectId))
+                if(context.SessionClassSubject.Any(x => x.SubjectId == lookup.SubjectId && x.ClientId == smsClientId))
                 {
                     res.Message.FriendlyMessage = "Subject  cannot be deleted";
                     return res;
@@ -148,7 +151,7 @@ namespace BLL.Services.SubjectServices
 
             var studentContactId = accessor.HttpContext.User.FindFirst(x => x.Type == "studentContactId")?.Value;
 
-            var student = context.StudentContact.Include(x => x.User).Include(d => d.SessionClass)
+            var student = context.StudentContact.Where(x=>x.ClientId == smsClientId).Include(x => x.User).Include(d => d.SessionClass)
                 .ThenInclude(s => s.SessionClassSubjects)
                 .ThenInclude(d => d.Subject).FirstOrDefault(d => d.StudentContactId == Guid.Parse(studentContactId));
 
@@ -160,7 +163,7 @@ namespace BLL.Services.SubjectServices
             }
 
             res.Result = student.SessionClass.SessionClassSubjects
-                .Where(e => e.Subject.Deleted == false && e.Subject.IsActive == true)
+                .Where(e => e.Subject.Deleted == false && e.Subject.IsActive == true && e.ClientId == smsClientId)
                 .Select(a => new DropdownSelect { 
                     Value = a.SubjectId.ToString().ToLower(), 
                     Name = a.Subject.Name,
@@ -175,9 +178,9 @@ namespace BLL.Services.SubjectServices
         APIResponse<Guid> ISubjectService.GetSubjectTeacher(Guid subjectId)
         {
             var stdId = accessor.HttpContext.User.FindFirst(d => d.Type == "studentContactId")?.Value;
-            var student = context.StudentContact.FirstOrDefault(s => s.StudentContactId == Guid.Parse(stdId));
+            var student = context.StudentContact.FirstOrDefault(s => s.ClientId == smsClientId && s.StudentContactId == Guid.Parse(stdId));
             var res = new APIResponse<Guid>();
-            var result = context.SessionClassSubject.FirstOrDefault(d => d.SubjectId == subjectId && d.SessionClassId == student.SessionClassId).SubjectTeacherId;
+            var result = context.SessionClassSubject.FirstOrDefault(d => d.ClientId == smsClientId && d.SubjectId == subjectId && d.SessionClassId == student.SessionClassId).SubjectTeacherId;
             res.Result = result;
             res.IsSuccessful = true;
             return res;
--- BLL/Services/TimetableServices/TimeTableService.cs
+++ BLL/Services/TimetableServices/TimeTableService.cs
@@ -18,18 +18,20 @@ namespace SMP.BLL.Services.TimetableServices
     {
         private readonly DataContext context;
         private readonly IHttpContextAccessor accessor;
+        private readonly string smsClientId;
         public TimeTableService(DataContext context, IHttpContextAccessor accessor)
         {
             this.context = context;
             this.accessor = accessor;
+            smsClientId = accessor.HttpContext.User.FindFirst(x => x.Type == "smsClientId")?.Value;
         }
 
         async Task<APIResponse<List<GetApplicationLookups>>> ITimeTableService.GetAllActiveClassesAsync()
         {
             var res = new APIResponse<List<GetApplicationLookups>>();
-            var activeClasses =  context.ClassLookUp.Where(d => d.Deleted != true && d.IsActive == true);
+            var activeClasses =  context.ClassLookUp.Where(d => d.ClientId == smsClientId && d.Deleted != true && d.IsActive == true);
 
-            if (activeClasses.Count() == context.ClassTimeTable.Count())
+            if (activeClasses.Count() == context.ClassTimeTable.Where(x => x.ClientId == smsClientId).Count())
             {
                res.Result = await activeClasses.Select(a => new GetApplicationLookups
                 {
@@ -42,7 +44,7 @@ namespace SMP.BLL.Services.TimetableServices
                 res.IsSuccessful = true;
                 return res;
             }
-            var noneAddedClassIds = context.ClassLookUp.Where(s => !context.ClassTimeTable.Select(d => d.ClassId).AsEnumerable().Contains(s.ClassLookupId)
+            var noneAddedClassIds = context.ClassLookUp.Where(s => s.ClientId == smsClientId && !context.ClassTimeTable.Select(d => d.ClassId).AsEnumerable().Contains(s.ClassLookupId)
             && s.Deleted != true && s.IsActive == true).Select(s => s.ClassLookupId).ToList();
 
             if (noneAddedClassIds.Any())
@@ -86,7 +88,7 @@ namespace SMP.BLL.Services.TimetableServices
                 await context.ClassTimeTableDay.AddAsync(req);
                 await context.SaveChangesAsync();
 
-                var classTimes = context.ClassTimeTableTime.Where(d => d.ClassTimeTableId == Guid.Parse(request.ClassTimeTableId)).AsEnumerable();
+                var classTimes = context.ClassTimeTableTime.Where(d => d.ClientId == smsClientId && d.ClassTimeTableId == Guid.Parse(request.ClassTimeTableId)).AsEnumerable();
                 if (classTimes.Any())
                 {
                     foreach (var time in classTimes)
@@ -129,7 +131,7 @@ namespace SMP.BLL.Services.TimetableServices
                 await context.ClassTimeTableTime.AddAsync(req);
                 await context.SaveChangesAsync();
 
-                var classDays = context.ClassTimeTable.Where(d => d.ClassId == Guid.Parse(request.ClassId)).SelectMany(s => s.Days).AsEnumerable();
+                var classDays = context.ClassTimeTable.Where(d => d.ClientId == smsClientId && d.ClassId == Guid.Parse(request.ClassId)).SelectMany(s => s.Days).AsEnumerable();
                 if (classDays.Any())
                 {
                     foreach (var day in classDays)
@@ -161,7 +163,7 @@ namespace SMP.BLL.Services.TimetableServices
             var res = new APIResponse<UpdateClassTimeTableTime>();
             try
             {
-                var time = context.ClassTimeTableTime.FirstOrDefault(x => x.ClassTimeTableTimeId == Guid.Parse(request.ClassTimeTableTimeId));
+                var time = context.ClassTimeTableTime.FirstOrDefault(x => x.ClientId == smsClientId && x.ClassTimeTableTimeId == Guid.Parse(request.ClassTimeTableTimeId));
 
                 time.Start = request.Start;
                 time.End = request.End;
@@ -186,7 +188,7 @@ namespace SMP.BLL.Services.TimetableServices
             var res = new APIResponse<UpdateClassTimeTableDay>();
             try
             {
-                var day = context.ClassTimeTableDay.FirstOrDefault(d => d.ClassTimeTableDayId == Guid.Parse(request.ClassTimeTableDayId));
+                var day = context.ClassTimeTableDay.FirstOrDefault(d => d.ClientId == smsClientId && d.ClassTimeTableDayId == Guid.Parse(request.ClassTimeTableDayId));
                 if(day == null)
                 {
                     res.Message.FriendlyMessage = Messages.FriendlyNOTFOUND;
@@ -214,7 +216,7 @@ namespace SMP.BLL.Services.TimetableServices
 
             try
             {
-                var req = context.ClassTimeTableTimeActivity.FirstOrDefault(d => d.ClassTimeTableTimeActivityId == Guid.Parse(request.ActivityId));
+                var req = context.ClassTimeTableTimeActivity.FirstOrDefault(d => d.ClientId == smsClientId && d.ClassTimeTableTimeActivityId == Guid.Parse(request.ActivityId));
                 if(req is not null)
                 {
                     req.Activity = request.Activity;
@@ -243,10 +245,10 @@ namespace SMP.BLL.Services.TimetableServices
             var res = new APIResponse<GetClassTimeActivity>();
 
             var result = await context.ClassTimeTable
+                .Where(d => d.ClientId == smsClientId && d.Deleted == false && d.ClassId == classId)
                 .Include(s => s.Class)
                 .Include(s => s.Days).ThenInclude(s => s.Activities).ThenInclude(d => d.Day)
                  .Include(s => s.Times).ThenInclude(d => d.Activities).ThenInclude(d => d.Day)
-                .Where(d => d.Deleted == false && d.ClassId == classId)
                 .Select(f => new GetClassTimeActivity(f)).FirstOrDefaultAsync();
 
             res.Message.FriendlyMessage = Messages.GetSuccess;
@@ -258,9 +260,9 @@ namespace SMP.BLL.Services.TimetableServices
         public async Task<APIResponse<List<GetClassTimeActivityByDay>>> GetClassTimeActivityByDayAsync(string day)
         {
             var res = new APIResponse<List<GetClassTimeActivityByDay>>();
-            var classList = context.ClassTimeTable.Include(d => d.Class).Where(x => x.Deleted == false).ToList();
+            var classList = context.ClassTimeTable.Where(x => x.ClientId == smsClientId && x.Deleted == false).Include(d => d.Class).ToList();
 
-            var result = await context.ClassTimeTable
+            var result = await context.ClassTimeTable.Where(x => x.ClientId == smsClientId)
                 .Include(s => s.Days)
                  .Include(s => s.Times).ThenInclude(d => d.Activities)
                 .Where(d => d.Deleted == false && d.Days.Select(d => d.Day).Contains(day))
@@ -276,7 +278,7 @@ namespace SMP.BLL.Services.TimetableServices
             var res = new APIResponse<SingleDelete>();
             try
             {
-                var classDays = context.ClassTimeTableTime.Include(d => d.Activities).FirstOrDefault(d => d.ClassTimeTableTimeId == Guid.Parse(request.Item));
+                var classDays = context.ClassTimeTableTime.Where(x=>x.ClientId == smsClientId).Include(d => d.Activities).FirstOrDefault(d => d.ClassTimeTableTimeId == Guid.Parse(request.Item));
                 context.ClassTimeTableTime.Remove(classDays); 
                 await context.SaveChangesAsync();
                 res.Result = request;
@@ -295,7 +297,7 @@ namespace SMP.BLL.Services.TimetableServices
             var res = new APIResponse<SingleDelete>();
             try
             {
-                var day = context.ClassTimeTableDay.Include(d => d.Activities).FirstOrDefault(d => d.ClassTimeTableDayId == Guid.Parse(request.Item));
+                var day = context.ClassTimeTableDay.Where(x=>x.ClientId == smsClientId).Include(d => d.Activities).FirstOrDefault(d => d.ClassTimeTableDayId == Guid.Parse(request.Item));
                 if(day is not null)
                 {
                     if(day.Activities.Any())
@@ -318,7 +320,7 @@ namespace SMP.BLL.Services.TimetableServices
         public async Task<APIResponse<List<GetClassDays>>> GetAllClassDaysAsync()
         {
             var res = new APIResponse<List<GetClassDays>>();
-            res.Result = await context.ClassTimeTableDay.Distinct().Select(d => new GetClassDays(d)).ToListAsync();
+            res.Result = await context.ClassTimeTableDay.Where(x => x.ClientId == smsClientId).Distinct().Select(d => new GetClassDays(d)).ToListAsync();
             res.Message.FriendlyMessage = Messages.GetSuccess;
             res.IsSuccessful = true;
             return res;
@@ -330,8 +332,8 @@ namespace SMP.BLL.Services.TimetableServices
             var studentContactId = accessor.HttpContext.User.FindFirst(d => d.Type == "studentContactId")?.Value;
             if (!string.IsNullOrEmpty(studentContactId))
             {
-                var studentAct = context.StudentContact.Include(s => s.SessionClass).FirstOrDefault(d => d.StudentContactId == Guid.Parse(studentContactId));
-                var result = await context.ClassTimeTable
+                var studentAct = context.StudentContact.Where(x => x.ClientId == smsClientId).Include(s => s.SessionClass).FirstOrDefault(d => d.StudentContactId == Guid.Parse(studentContactId));
+                var result = await context.ClassTimeTable.Where(x => x.ClientId == smsClientId)
                   .Include(s => s.Class)
                   .Include(s => s.Days).ThenInclude(s => s.Activities).ThenInclude(d => d.Day)
                    .Include(s => s.Times).ThenInclude(d => d.Activities).ThenInclude(d => d.Day)
@@ -348,7 +350,7 @@ namespace SMP.BLL.Services.TimetableServices
         async Task<APIResponse<GetClassTimeActivity>> ITimeTableService.GetClassTimeTableByParentsAsync(Guid classlkpId)
         {
             var res = new APIResponse<GetClassTimeActivity>();
-            var result = await context.ClassTimeTable
+            var result = await context.ClassTimeTable.Where(x => x.ClientId == smsClientId)
               .Include(s => s.Class)
               .Include(s => s.Days).ThenInclude(s => s.Activities).ThenInclude(d => d.Day)
                .Include(s => s.Times).ThenInclude(d => d.Activities).ThenInclude(d => d.Day)
