c5b02770e7814849400ac2c254bdd9317da8b53c
flave1 <favouremmanuel433@gmail.com>
2023-01-30 09:27:28 +0100

    ..

--- BLL/Services/TeacherServices/TeacherService.cs
+++ BLL/Services/TeacherServices/TeacherService.cs
@@ -316,5 +316,66 @@ namespace SMP.BLL.Services.TeacherServices
             return res;
         }
 
+        async Task<APIResponse<string>> ITeacherService.CreateAdminAsync(UserCommand request)
+        {
+            var res = new APIResponse<string>();
+            try
+            {
+                if (userManager.Users.Any(e => e.Email.ToLower().Trim().Contains(request.Email.ToLower().Trim())))
+                {
+                    res.Message.FriendlyMessage = "Teacher With Email Has Already been Added";
+                    return res;
+                }
+                var user = new AppUser
+                {
+                    UserName = request.Email,
+                    Active = true,
+                    Deleted = false,
+                    CreatedOn = DateTime.UtcNow,
+                    CreatedBy = "",
+                    Email = request.Email,
+                    UserType = (int)UserTypes.Teacher,
+                    EmailConfirmed = false,
+                    DOB = request.DOB,
+                    FirstName = request.FirstName,
+                    LastName = request.LastName,
+                    MiddleName = request.MiddleName,
+                    Phone = request.Phone,
+                    PhoneNumber = request.Phone,
+                    PhoneNumberConfirmed = false,
+                    Photo = ""
+                };
+                var result = await userManager.CreateAsync(user, UserConstants.PASSWORD);
+                if (!result.Succeeded)
+                {
+                    res.Message.FriendlyMessage = result.Errors.FirstOrDefault().Description;
+                    return res;
+                }
+                var addTorole = await userManager.AddToRoleAsync(user, DefaultRoles.TEACHER);
+                if (!addTorole.Succeeded)
+                {
+                    res.Message.FriendlyMessage = addTorole.Errors.FirstOrDefault().Description;
+                    return res;
+                }
+
+                context.Teacher.Add(new Teacher { UserId = user.Id, Status = (int)TeacherStatus.Active });
+                await context.SaveChangesAsync();
+
+                //await SendEmailToTeacherOnCreateAsync(user);
+                res.IsSuccessful = true;
+                res.Message.FriendlyMessage = "Successfully added a staff";
+                res.Result = "Success";
+                return res;
+
+            }
+            catch (ArgumentException ex)
+            {
+                res.Message.FriendlyMessage = ex.Message;
+                return res;
+            }
+        }
+
+
+
     }
 }
