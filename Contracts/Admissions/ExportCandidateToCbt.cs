cc44b59be6303fcfa677fc519ab0c7ac52b4b9db
flave1 <favouremmanuel433@gmail.com>
2023-03-16 09:42:36 +0100

    ..

--- Contracts/Admissions/ExportCandidateToCbt.cs
+++ Contracts/Admissions/ExportCandidateToCbt.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SMP.Contracts.Admissions
+﻿namespace SMP.Contracts.Admissions
 {
     public class ExportCandidateToCbt
     {
